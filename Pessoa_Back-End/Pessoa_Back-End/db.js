52ba50e2bb2d7b5b90a4e8313a1aa581bb630e2c
jamili <jamili.amaral@estudante.ifms>
2023-06-12 15:55:06 -0400

    Initial commit

---
+++ Pessoa_Back-End/Pessoa_Back-End/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://rejmgzgz:VMIhMBsY5VNW6R_rJiP7IGaAPaOBb3Ez@silly.db.elephantsql.com/rejmgzgz'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
