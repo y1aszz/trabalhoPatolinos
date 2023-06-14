346d79abf6b73d03cb4d9d64cc3a6790673881b6
jamili <jamili.amaral@estudante.ifms>
2023-06-13 22:22:14 -0400

    alterando titulo

--- Pessoa_Front-End/Pessoa_Front-End/src/app/layout.js
+++ Pessoa_Front-End/Pessoa_Front-End/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Projeto aula',
+  title: 'Livraria online',
   description: 'Simulando consumo de api com JsonServer',
 }
 
