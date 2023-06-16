55c71966e3866db1517422b6ee986cc95d64ce98
jenzinha <jamili.amaral@estudante.ifms.edu.br>
2023-06-16 01:07:47 -0300

    header concluido

--- front/front/src/app/layout.js
+++ front/front/src/app/layout.js
@@ -15,11 +15,12 @@ export default function RootLayout({ children }) {
       <body>
         <header className='header'>
           <img className='logo' src='https://img.freepik.com/vetores-premium/gato-bonito-lendo-um-livro-conceito-de-desenho-animado-animal-isolado_556653-3388.jpg?w=360'></img>
-          <ul>
-         <li><Link href="/cadastro" className='voltar' > CADASTRAR </Link></li>
-         <li><a href='/'>VOLTAR</a></li>
+          <ul> 
+          <li><Link href="/cadastro" className='voltar' > CADASTRAR </Link></li>
+         <li className='voltando'><a href='/'>VOLTAR</a></li>
          </ul>
          <a href=''><img className='carrinho' src='https://cdn.icon-icons.com/icons2/1830/PNG/512/cart2-115853_115816.png'></img></a>
+      
         </header>
         {children}</body>
     </html>
