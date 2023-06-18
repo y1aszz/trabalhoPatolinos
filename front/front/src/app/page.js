94a492a8a3ccdedbe6593a4e21087bb10a6ca3ed
jenzinha <jamili.amaral@estudante.ifms.edu.br>
2023-06-18 11:08:52 -0300

    mudando pra roxinho

--- front/front/src/app/page.js
+++ front/front/src/app/page.js
@@ -22,9 +22,32 @@ export default async function Home() {
          
           <li><p style={{fontWeight: 'bold', fontSize: 16, marginTop:10}}>{produtos.titulo}</p></li>
            <li><Link href={`/produto/${produtos.codigo}`}><img style={{width: 145}} src={produtos.imagem}/></Link></li>
-          <li>{produtos.cadastro}</li>
-          <li>R${produtos.preco}</li>
-         <li><Link className='verMais' href={`/produto/${produtos.codigo}`}>VER MAIS</Link></li>
+          
+          <li style={{
+                  color: '#252424b0',
+                  fontSize: 14,
+                  fontWeight: 700,
+                  textAlign: 'center',
+                  textTransform: 'uppercase',
+          }}>{produtos.cadastro}</li>
+
+          <li style={{
+             color: '#252424b0',
+             fontSize: 14,
+             fontWeight: 700,
+             textAlign: 'center',
+             textTransform: 'uppercase',
+          }}>R${produtos.preco}</li>
+
+         <li><Link style={{
+           color: 'blueviolet',
+           fontSize: 14,
+           fontWeight: 700,
+           textAlign: 'center',
+           marginLeft:77,
+           textTransform: 'uppercase',
+         }}
+        href={`/produto/${produtos.codigo}`}>VER MAIS</Link></li>
          
           </ul>
         </div>
