c0e7b4bcf9ce1a2d8ad43876e8b771315ba2f5e4
y1aszz <yasmim.sordi@estudante.ifms.edu.br>
2023-08-02 10:41:26 -0300

    tentando colocar vercel nessa bomba

--- back/back/index.js
+++ back/back/index.js
@@ -5,6 +5,8 @@ const cors= require('cors');
 const app = express();
 app.use(cors());
 
+const port = process.env.PORT || 3004
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
