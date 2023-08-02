9a8384fb68f2b470f4d9b1c5166bf2d1b8541756
jenzinha <101286269+jenzinha@users.noreply.github.com>
2023-08-01 23:26:05 -0400

    Update page.js

--- front/front/src/app/page.js
+++ front/front/src/app/page.js
@@ -6,7 +6,7 @@ import styles from './page.module.css'
 
 export default async function Home() {
 
-  const req = await fetch("http://localhost:3004/produtos", {
+ // const req = await fetch("http://localhost:3004/produtos", {
     cache: "no-cache"
   });
   const produtos = await req.json();
